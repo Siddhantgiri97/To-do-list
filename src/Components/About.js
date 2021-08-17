5864e569c6c4a41ae1b1121ed12f8fdc9e3b861b
Siddhant <siddhantgiri97@gmail.com>
2021-08-17 21:17:07 +0530

    first commit

---
+++ src/Components/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <h2>About</h2>
+            <hr />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis expedita ab accusamus totam libero repudiandae dolorum corrupti assumenda, rem illum aliquid! Dolorum exercitationem totam in possimus fuga debitis placeat magni qui quas. Incidunt, veritatis?</p>
+        </div>
+    )
+}
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+    
+
+    return (
+        <footer className="bg-dark text-light" >
+            <p className="text-center py-3">
+                Copyright &copy; ktgservices.com
+            </p>
+
+        </footer>
+    )
+}
